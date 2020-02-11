5a6728f135b840d907cc41d350bf09db33b0ab83
aaltamirano1 <aa250839@ncr.com>
2020-02-10 23:18:59 -0500

    getting titles of tabs opened with a while loop

--- src/com/myseleniumhw/Main.java
+++ src/com/myseleniumhw/Main.java
@@ -1,9 +1,14 @@
 package com.myseleniumhw;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -12,13 +17,29 @@ public class Main {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-        // one way to get all links in footer:
-        //System.out.println(driver.findElements(By.cssSelector("div#gf-BIG a")).size());
-
-        // other way to get all links in footer: (limiting webdriver scope)
+        // identify 'Discount Coupons' column of links in the footer.
         WebElement footer = driver.findElement(By.id("gf-BIG"));
-        int linkCount = footer.findElements(By.tagName("a")).size();
-        System.out.println(linkCount);
-        driver.close();
+        WebElement discCouponsColumn = footer.findElements(By.tagName("td")).get(0);
+
+        // iterate over these links.
+        List<WebElement> discCouponsLinks = discCouponsColumn.findElements(By.cssSelector("li>a"));
+        for(int i = 0; i< ((List) discCouponsLinks).size(); i++){
+
+            // open each link.
+            String openLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
+            //System.out.println((i+1)+". "+discCouponsLinks.get(i).getText()); //print a numbered list of the links opened.
+            discCouponsLinks.get(i).sendKeys(openLink);
+        }
+
+        // get the title from each window/tab and print a numbered list of the titles.
+        Set<String> ids = driver.getWindowHandles();
+        Iterator<String> handles = ids.iterator();
+        int count = 1;
+        while(handles.hasNext()){
+            driver.switchTo().window(handles.next());
+            System.out.println(count+". "+driver.getTitle());
+            count++;
+        }
+
     }
 }
