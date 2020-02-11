53ecc689e6655ceca5cfa33c96c43bd85459047f
aaltamirano1 <aa250839@ncr.com>
2020-02-10 22:01:11 -0500

    handling frames & draggable elements

--- src/com/myseleniumhw/Main.java
+++ src/com/myseleniumhw/Main.java
@@ -2,7 +2,9 @@ package com.myseleniumhw;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.Iterator;
 import java.util.Set;
@@ -12,27 +14,19 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Angel-Altamirano\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://jqueryui.com/droppable/");
 
-        //click 'multiple windows'.
-        driver.get("http://the-internet.herokuapp.com/");
-        driver.findElement(By.xpath("//*[@id='content']/ul/li/a[text()='Multiple Windows']")).click();
+        //switch to frame context
+        WebElement iframe = driver.findElement(By.cssSelector("iframe.demo-frame"));
+        driver.switchTo().frame(iframe);
 
-        //click 'click here'.
-        driver.findElement(By.xpath("//div[@id='content']/div/a[text()='Click Here']")).click();
+        //inistantiate actions class.
+        Actions doIt = new Actions(driver);
 
-        Set<String> ids = driver.getWindowHandles();
-        Iterator<String> handles = ids.iterator();
-        String parentId = handles.next();
-        String childId = handles.next();
-
-
-        // in the new window, capture 'New Window' text
-        driver.switchTo().window(childId);
-        System.out.println(driver.findElement(By.cssSelector("div.example h3")).getText());
-
-        //then go back to the parent window and capture the 'opening a new window' text.
-        driver.switchTo().window(parentId);
-        System.out.println(driver.findElement(By.cssSelector("div#content h3")).getText());
+        //drag and drop element.
+        WebElement draggableElement = driver.findElement(By.id("draggable"));
+        WebElement dropPoint = driver.findElement(By.id("droppable"));
+        doIt.dragAndDrop(draggableElement, dropPoint).build().perform();
 
     }
 }
