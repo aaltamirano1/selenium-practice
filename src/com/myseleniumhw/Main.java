5d00e96d0351ad0e0a0741990a76c512a37b62ef
aaltamirano1 <aa250839@ncr.com>
2020-02-16 11:19:01 -0500

    filling out a web form and working with alerts

--- src/com/myseleniumhw/Main.java
+++ src/com/myseleniumhw/Main.java
@@ -1,45 +1,60 @@
 package com.myseleniumhw;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.Assert;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Angel-Altamirano\\Downloads\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Angel-Altamirano\\Documents\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-        // identify 'Discount Coupons' column of links in the footer.
-        WebElement footer = driver.findElement(By.id("gf-BIG"));
-        WebElement discCouponsColumn = footer.findElements(By.tagName("td")).get(0);
 
-        // iterate over these links.
-        List<WebElement> discCouponsLinks = discCouponsColumn.findElements(By.cssSelector("li>a"));
-        for(int i = 0; i< ((List) discCouponsLinks).size(); i++){
+        // avoids timeout receiving message from renderer.
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
 
-            // open each link.
-            String openLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
-            //System.out.println((i+1)+". "+discCouponsLinks.get(i).getText()); //print a numbered list of the links opened.
-            discCouponsLinks.get(i).sendKeys(openLink);
-        }
+        // random number generated.
+        Random rand = new Random();
+        int checkboxIndex = rand.nextInt(3);
+        System.out.println("Option Number "+checkboxIndex+" generated.");
 
-        // get the title from each window/tab and print a numbered list of the titles.
-        Set<String> ids = driver.getWindowHandles();
-        Iterator<String> handles = ids.iterator();
-        int count = 1;
-        while(handles.hasNext()){
-            driver.switchTo().window(handles.next());
-            System.out.println(count+". "+driver.getTitle());
-            count++;
-        }
+        // randomly click one of the checkboxes.
+        WebElement randomCheckboxLabel = driver.findElements(By.xpath("//input[@type='checkbox']/parent::label")).get(checkboxIndex);
+        String checkboxLabelText = randomCheckboxLabel.getText().trim();
+        randomCheckboxLabel.findElement(By.tagName("input")).click();
+        System.out.println("Checkbox with text '"+checkboxLabelText+"' clicked.");
 
+        // click dropdown.
+        WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
+        dropdown.click();
+        System.out.println("Dropdown menu clicked.");
+
+        // select a dropdown option with the same text as the checkbox label.
+        WebElement dropdownOption = dropdown.findElement(By.xpath("//option[contains(text(),'"+checkboxLabelText+"')]"));
+        dropdownOption.click();
+        System.out.println("Selected dropdown option with text '"+dropdownOption.getText()+"'.");
+
+        // type the checkbox label text inth the alert text field.
+        driver.findElement(By.id("name")).sendKeys(checkboxLabelText);
+        System.out.println("Typed '"+checkboxLabelText+"' into alert text input.");
+
+        // click alert button.
+        driver.findElement(By.id("alertbtn")).click();
+        System.out.println("Alert button clicked.");
+
+        // check if alert text contains the checkbox label text.
+        String alertText = driver.switchTo().alert().getText();
+        boolean alertContainsCheckboxLabelText = alertText.contains(checkboxLabelText);
+        Assert.assertTrue(alertContainsCheckboxLabelText);
     }
 }
